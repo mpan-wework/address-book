b775a160723165fc63e762bef5bb07a0347fca9c
Mike Pan <mike.pan@wework.com>
2020-08-20 00:27:10 +0800

    feat: add delete record (#2)

--- src/antd/AddressBook.js
+++ src/antd/AddressBook.js
@@ -1,24 +1,67 @@
-import React, { useState } from 'react';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import { Button, Table } from 'antd';
 import { t } from '../utils/i18n';
+import client from '../utils/client';
 import styles from './AddressBook.module.css';
 
+const initialDataSource = [
+  {
+    id: 501,
+    name: 'Khall Zhang',
+    location: 'Shanghai',
+    office: 'C-103',
+    phoneOffice: 'x55778',
+    phoneCell: '650-353-1239',
+  },
+  {
+    id: 502,
+    name: 'Khall Zhang',
+    location: 'Shanghai',
+    office: 'C-103',
+    phoneOffice: 'x55778',
+    phoneCell: '650-353-1239',
+  },
+];
+
 const AddressBook = () => {
-  const [dataSource] = useState([
-    {
-      id: 501,
-      name: 'Khall Zhang',
-      location: 'Shanghai',
-      office: 'C-103',
-      phoneOffice: 'x55778',
-      phoneCell: '650-353-1239',
-    }
-  ]);
+  const [dataSource, setDataSource] = useState(() => initialDataSource);
+
+  const [selectedRowKeys, setSelectedRowKeys] = useState([]);
+
+  const rowSelection = useMemo(
+    () => ({
+      selectedRowKeys,
+      onChange: setSelectedRowKeys,
+    }),
+    [selectedRowKeys, setSelectedRowKeys],
+  );
+
+  useEffect(() => {
+    setSelectedRowKeys(
+      (keys) =>
+        dataSource
+          .map((row) => row.id)
+          .filter((key) => keys.includes(key)),
+    );
+  }, [dataSource, setSelectedRowKeys]);
+
+  const deleteRows = useCallback(
+    async () => {
+      await client.deleteRecords(selectedRowKeys);
+      setDataSource((records) => records.filter((record) => !selectedRowKeys.includes(record.id)));
+    },
+    [selectedRowKeys, setDataSource],
+  );
 
   return (
     <div className={'addressBook'}>
       <div className={styles.title}>{t('Address Book')}</div>
-      <Table dataSource={dataSource} rowKey={'id'} pagination={false}>
+      <Table
+        dataSource={dataSource}
+        rowKey={'id'}
+        pagination={false}
+        rowSelection={rowSelection}
+      >
         <Table.Column key={'id'} title={t('ID')} dataIndex={'id'} />
         <Table.Column key={'name'} title={t('Name')} dataIndex={'name'} />
         <Table.Column key={'location'} title={t('Location')} dataIndex={'location'} />
@@ -30,7 +73,13 @@ const AddressBook = () => {
       </Table>
       <div className={styles.buttons}>
         <div className={styles.left}>
-          <Button className={styles.delete}>{t('Delete')}</Button>
+          <Button
+            className={styles.delete}
+            disabled={selectedRowKeys.length === 0}
+            onClick={deleteRows}
+          >
+            {t('Delete')}
+          </Button>
         </div>
         <div className={styles.right}>
           <Button className={styles.update}>{t('Update')}</Button>
---
+++ src/utils/client.js
@@ -0,0 +1,32 @@
+const dummyRequest = (data) => new Promise((resolve) => {
+  setTimeout(() => {
+    resolve(data);
+  }, 2000);
+})
+
+const createClient = () => {
+  const createRecord = async (row) => {
+    console.info('createRecord:', row)
+    return dummyRequest({ ...row, id: Date.now() });
+  };
+
+  const updateRecord = async (id, row) => {
+    console.info('updateRecord:', id, row);
+    return dummyRequest(row);
+  };
+
+  const deleteRecords = async (ids) => {
+    console.info('deleteRecords:', ids);
+    return dummyRequest(null);
+  };
+
+  return {
+    createRecord,
+    updateRecord,
+    deleteRecords,
+  };
+};
+
+const client = createClient();
+
+export default client;
