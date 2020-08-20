d0fb6c88d6823169b913545ff4dd11aacf336787
Mike Pan <mike.pan@wework.com>
2020-08-20 23:32:58 +0800

    Revert "feat: add create record (#3)"
    This reverts commit 1660e537e0067f4d5916b4aa568981eccf82fc95.

--- src/antd/AddressBook.js
+++ src/antd/AddressBook.js
@@ -23,35 +23,17 @@ const initialDataSource = [
   },
 ];
 
-const ID_ADD = -1;
-const initialAddRow = {
-  id: ID_ADD,
-  name: '',
-  location: '',
-  office: '',
-  phoneOffice: '',
-  phoneCell: '',
-  rowClassName: styles.addRow,
-  editable: true,
-};
-
 const AddressBook = () => {
   const [dataSource, setDataSource] = useState(() => initialDataSource);
-  const [addRow, setAddRow] = useState(() => initialAddRow);
 
   const [selectedRowKeys, setSelectedRowKeys] = useState([]);
-
-  const getCheckboxProps = useCallback((record) => ({
-    disabled: record.rowClassName === styles.addRow,
-  }), []);
 
   const rowSelection = useMemo(
     () => ({
       selectedRowKeys,
       onChange: setSelectedRowKeys,
-      getCheckboxProps,
     }),
-    [selectedRowKeys, getCheckboxProps, setSelectedRowKeys],
+    [selectedRowKeys, setSelectedRowKeys],
   );
 
   useEffect(() => {
@@ -63,8 +45,6 @@ const AddressBook = () => {
     );
   }, [dataSource, setSelectedRowKeys]);
 
-  const rowClassName = useCallback((record) => record.rowClassName || '', []);
-
   const deleteRows = useCallback(
     async () => {
       await client.deleteRecords(selectedRowKeys);
@@ -73,100 +53,22 @@ const AddressBook = () => {
     [selectedRowKeys, setDataSource],
   );
 
-  const cellChange = useCallback((id, key) => (e) => {
-    const value = e.target.value;
-
-    if (id === ID_ADD) {
-      setAddRow((prev) => ({
-        ...prev,
-        [key]: value,
-      }))
-    }
-  }, [setAddRow]);
-
-  const editableCell = useCallback(
-    (key) => (cell, record) => (record.editable) ? (
-      <input
-        className={styles.edit}
-        value={cell}
-        onChange={cellChange(record.id, key)}
-      />
-    ) : (
-      <div>{cell}</div>
-    ),
-    [cellChange],
-  );
-
-  const invalidAddRow = useMemo(
-    () => !Object.keys(addRow).reduce((acc, key) => acc && addRow[key] !== '', true),
-    [addRow],
-  );
-
-  const addNewRow = useCallback(
-    async () => {
-      const newRow = await client.createRecord({
-        ...addRow,
-        rowClassName: '',
-        editable: '',
-      });
-      setDataSource((prev) => prev.concat(newRow));
-      setAddRow(initialAddRow);
-    },
-    [addRow, setDataSource, setAddRow],
-  );
-
-  const dataWithAdd = useMemo(() => dataSource.concat(addRow), [dataSource, addRow]);
-
   return (
     <div className={'addressBook'}>
       <div className={styles.title}>{t('Address Book')}</div>
       <Table
-        dataSource={dataWithAdd}
+        dataSource={dataSource}
         rowKey={'id'}
         pagination={false}
         rowSelection={rowSelection}
-        rowClassName={rowClassName}
       >
-        <Table.Column
-          key={'id'}
-          title={t('ID')}
-          dataIndex={'id'}
-          render={(value) => <div>{value}</div>}
-        />
-        <Table.Column
-          key={'name'}
-          title={t('Name')}
-          dataIndex={'name'}
-          render={editableCell('name')}
-        />
-        <Table.Column
-          key={'location'}
-          title={t('Location')}
-          dataIndex={'location'}
-          render={editableCell('location')}
-        />
-        <Table.Column
-          key={'office'}
-          title={t('Office')}
-          dataIndex={'office'}
-          render={editableCell('office')}
-        />
-        <Table.ColumnGroup
-          key={'phone'}
-          title={t('Phone')}
-        >
-          <Table.Column
-            key={'phoneOffice'}
-            title={t('Office')}
-            dataIndex={'phoneOffice'}
-            render={editableCell('phoneOffice')}
-          />
-          <Table.Column
-            key={'phoneCell'}
-            title={t('Cell')}
-            dataIndex={'phoneCell'}
-            render={editableCell('phoneCell')}
-          />
+        <Table.Column key={'id'} title={t('ID')} dataIndex={'id'} />
+        <Table.Column key={'name'} title={t('Name')} dataIndex={'name'} />
+        <Table.Column key={'location'} title={t('Location')} dataIndex={'location'} />
+        <Table.Column key={'office'} title={t('Office')} dataIndex={'office'} />
+        <Table.ColumnGroup key={'phone'} title={t('Phone')}>
+          <Table.Column key={'phoneOffice'} title={t('Office')} dataIndex={'phoneOffice'} />
+          <Table.Column key={'phoneCell'} title={t('Cell')} dataIndex={'phoneCell'} />
         </Table.ColumnGroup>
       </Table>
       <div className={styles.buttons}>
@@ -181,13 +83,7 @@ const AddressBook = () => {
         </div>
         <div className={styles.right}>
           <Button className={styles.update}>{t('Update')}</Button>
-          <Button
-            className={styles.add}
-            disabled={invalidAddRow}
-            onClick={addNewRow}
-          >
-            {t('Add')}
-          </Button>
+          <Button className={styles.add}>{t('Add')}</Button>
         </div>
       </div>
     </div>
