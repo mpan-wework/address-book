7b9e082721ccce4b88e9aeabee96928957f6e879
Mike Pan <mike.pan@wework.com>
2020-08-24 01:47:13 +0800

    chore: build (#7)

---
+++ docs/precache-manifest.bb66020fc8df5e291bfa08799e784315.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6baec2b1630ca7285d920e21cb88a8ca",
+    "url": "./index.html"
+  },
+  {
+    "revision": "508fda981dfde7bf4699",
+    "url": "./static/css/2.04ff9426.chunk.css"
+  },
+  {
+    "revision": "4fd1937d58106d7c9819",
+    "url": "./static/css/main.68b2c3ae.chunk.css"
+  },
+  {
+    "revision": "508fda981dfde7bf4699",
+    "url": "./static/js/2.88942e21.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.88942e21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fd1937d58106d7c9819",
+    "url": "./static/js/main.cc26cd2c.chunk.js"
+  },
+  {
+    "revision": "0d017eb8b4ff5ed2884e",
+    "url": "./static/js/runtime-main.2a7978e2.js"
+  }
+]);
